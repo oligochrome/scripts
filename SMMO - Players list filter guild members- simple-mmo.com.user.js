67ca36fdf87798caef5c17b5ad74046724e6675c
Oligochrome <55333841+oligochrome@users.noreply.github.com>
2023-07-20 02:13:33 +0100

    Update SMMO - Players list filter guild members- simple-mmo.com.user.js

--- SMMO - Players list filter guild members- simple-mmo.com.user.js
+++ SMMO - Players list filter guild members- simple-mmo.com.user.js
@@ -6,7 +6,6 @@
 // @version     1.0
 // @author      Ogliochrome
 // @description 4/14/2023, 12:53:32 AM
-/* @require     https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js*/
 // ==/UserScript==
 if(localStorage.getItem("lbhide") === "true"){
 let guilds = document.getElementsByClassName('text-indigo-600 hover:text-indigo-700')
