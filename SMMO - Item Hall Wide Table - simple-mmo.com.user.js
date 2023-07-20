a4e703ba2ad4947c8818e52efb6e774823cde03e
Oligochrome <55333841+oligochrome@users.noreply.github.com>
2023-07-20 02:26:54 +0100

    Update SMMO - Item Hall Wide Table - simple-mmo.com.user.js

--- SMMO - Item Hall Wide Table - simple-mmo.com.user.js
+++ SMMO - Item Hall Wide Table - simple-mmo.com.user.js
@@ -10,6 +10,7 @@
 // @version        1.0
 // @language       en
 // @description    09/05/2023, 17:01:25
+// @updateurl      https://github.com/oligochrome/scripts/raw/main/SMMO%20-%20Item%20Hall%20Wide%20Table%20-%20simple-mmo.com.user.js
 
 // ==/UserScript==
 let gt = document.body.getElementsByClassName('-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8')[0]
