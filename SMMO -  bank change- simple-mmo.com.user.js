bd9df8979ea935fd1706153e613dd05a20c43955
Oligochrome <55333841+oligochrome@users.noreply.github.com>
2023-07-20 03:36:17 +0100

    Update SMMO -  bank change- simple-mmo.com.user.js

--- SMMO -  bank change- simple-mmo.com.user.js
+++ SMMO -  bank change- simple-mmo.com.user.js
@@ -9,6 +9,7 @@
 // @version        1.0
 // @language       en
 // @description    09/05/2023, 23:31:18
+// @updateURL      https://github.com/oligochrome/scripts/raw/main/SMMO%20-%20%20bank%20change-%20simple-mmo.com.user.js
 
 // ==/UserScript==
 if(window.location.href === "https://web.simple-mmo.com/bank?withdraw"){document.querySelector("body > div.h-screen.flex.overflow-hidden.bg-gray-100 > div.flex.flex-col.w-0.flex-1.overflow-hidden.relative > main > div.web-app-container > div.sm\\:mx-auto.sm\\:w-full.sm\\:max-w-md > div > div:nth-child(7)").style.display = "block"
