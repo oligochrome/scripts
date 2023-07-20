e2ff3c5d40681dcf208829b4d8b846f096297231
Oligochrome <55333841+oligochrome@users.noreply.github.com>
2023-07-20 02:06:02 +0100

    Update SMMO -  qyests button bigger - simple-mmo.com.user.js

--- SMMO -  qyests button bigger - simple-mmo.com.user.js
+++ SMMO -  qyests button bigger - simple-mmo.com.user.js
@@ -6,7 +6,6 @@
 // @version     1.0
 // @author      Ogliochrome
 // @description 10/22/2022, 3:35:29 PM
-/* @require     https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js*/
 // ==/UserScript==
 document.querySelector("#questButton").style.height = "300px"
 document.querySelector("#questButton").style.width = "300px"
