2eff5cf86cbdbf6a3f364d1380bf165e250ad723
Oligochrome <55333841+oligochrome@users.noreply.github.com>
2023-07-27 21:55:21 +0100

    Update SMMO - Market item in title.user.js

--- SMMO - Market item in title.user.js
+++ SMMO - Market item in title.user.js
@@ -6,6 +6,7 @@
 // @version     1.0
 // @author      Ogliochrome
 // @description 7/27/2023, 9:53:38 PM
+// @updateURL   https://github.com/oligochrome/scripts/raw/main/SMMO - Market item in title.user.js
 // ==/UserScript==
 
 var title = document.getElementsByClassName('text-sm font-medium text-gray-900 dark:text-gray-100 truncate')[0].textContent.replace('\n                        ','').replace('\n                      ','')
